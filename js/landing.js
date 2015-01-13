ae56b4a2bcc4e53bf70712aa4dc32c429d60a7c8
mkeedlinger <mkeedlinger@gmail.com>
2015-01-13 00:55:27 -0800

    started jekyll site

---
+++ js/landing.js
@@ -0,0 +1,26 @@
+$(function () {
+    markdown();
+});
+
+function markdown () {
+    $('md').each(function (index, element) {
+        element = $(element);
+        element.html(marked(limitSpacing(element.html())));
+        if (element.data('id') === undefined) {
+            element.children().each(function (ind, ele) {
+                ele = $(ele);
+                ele.removeAttr('id');
+            });
+        };
+    });
+}
+
+function limitSpacing (text) {
+    var count = text.match(/^\s*/)[0].length - 1;
+
+    text = text.split('\n').slice(1, -1);
+    text = text.map(function (currentLine) {
+        return currentLine.slice(count);
+    });
+    return text.join('\n');
+}
